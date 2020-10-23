9c2a1fa558109bc6d3c780be1266dfa25bba6d04
matzhananar <anr.matzhn@gmail.com>
2020-10-24 03:06:06 +0600

    secondbla

--- labfragments/app/src/main/java/com/example/labfragments/MainActivity.kt
+++ labfragments/app/src/main/java/com/example/labfragments/MainActivity.kt
@@ -2,19 +2,22 @@ package com.example.labfragments
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.labfragments.R
 import com.example.labfragments.User
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.mails.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var user = ArrayList<User>()
-        for(i in 1..99){
-            user.add(User("Name"+i, "", "employee", 20+i))
+        for(i in 1..50){
+
+            user.add(User("Name"+i, "","write a message", "employee", 20+i))
         }
 
         val userListAdapter = UserListAdapter(user, this)
@@ -24,3 +27,5 @@ class MainActivity : AppCompatActivity() {
         recyclerview.adapter = userListAdapter
     }
 }
+
+
